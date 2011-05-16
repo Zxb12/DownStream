687f3e74cffabcfd582dd3c0598a15630992d2fe
Zxb12 <steel_fire@msn.com>
2011-05-16 22:25:21 +0200

    Traductions des boutons crees via l'API Windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <QtGui/QApplication>
+#include <QTranslator>
+#include <QLocale>
+#include <QLibraryInfo>
 #include "fenprincipale.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QString locale = QLocale::system().name().section('_', 0, 0);
+    QTranslator translator;
+    translator.load(QString("qt_") + locale, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    a.installTranslator(&translator);
+
     FenPrincipale w;
     w.show();
 
